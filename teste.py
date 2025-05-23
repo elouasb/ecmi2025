42826b603037891d96345bf1254c0be1d28cf368
elouasb <eloahpuertas@gmail.com>
2025-05-23 11:02:35 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -6,4 +6,4 @@ st.write("""
 APP ELOUASB
 *PRIMEIRO SITE APP*
 """)
-
+st.image("https://worker.bnsir.com.br/?width=1000&quality=30&format=webp&fit=cover&image=https://images-prd.bnsir.com.br/used/36700/7ed1581e64a533b07ca20297ace59eee_banner-small.webp")
